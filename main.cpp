fb712065f0e4b974a18088de32e53414d3033b26
andela-ydaniju <yusuf.daniju@andela.com>
2017-01-02 07:16:30 +0100

    first c++ application

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * 
+ * Copyright 2016 ydaniju
+ * 
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: ydaniju
+ *
+ * Created on January 2, 2017, 7:11 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
